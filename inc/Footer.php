246dca047bdda8adf1cc84d395fe2e762e5b5e17
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2019-06-23 16:39:04 -0300

    [UPD] Atualizando o inxex.html

--- inc/Footer.php
+++
@@ -1,9 +0,0 @@
-    </main> <!-- /container -->
-
-    <br>
-    <hr>
-    <footer class="container">
-        <p>&copy;2019 - Mathias Artur Schulz</p>
-    </footer>
-</body>
-</html>
--- index - Copia.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once "inc/Header.php";
-
-    header('Location: ViewCaminhao.class.php');
-
-    require_once "inc/Footer.php";
-?>
